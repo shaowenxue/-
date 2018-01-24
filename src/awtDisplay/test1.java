d1b1b3aa8aadaa9bd08447c42ed5e67270ba8fd1
shaowenxue <1096398722@qq.com>
2018-01-24 20:41:56 +0800

    First commit

---
+++ src/awtDisplay/test1.java
@@ -0,0 +1,27 @@
+package awtDisplay;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+
+public class test1 {
+  public static void main(String args[]) throws
+           AWTException, IOException {
+     // capture the whole screen
+     BufferedImage screencapture = new Robot().createScreenCapture(
+           new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()) );
+
+     // Save as JPEG
+     File file = new File("screencapture.jpg");
+     ImageIO.write(screencapture, "jpg", file);
+
+     // Save as PNG
+     // File file = new File("screencapture.png");
+     // ImageIO.write(screencapture, "png", file);
+  }
+}
+
